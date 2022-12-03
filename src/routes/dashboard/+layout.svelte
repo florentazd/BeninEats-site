bf3ab3cb36fe5d2e3cd4d4ec5929632482fc0f1c
florentazd <florent4real@yahoo.com>
2022-12-03 09:19:21 +0100

    Refonte du sidebar et démarrage de la page home

---
+++ src/routes/dashboard/+layout.svelte
@@ -0,0 +1,159 @@
+<script>
+	import { page } from "$app/stores";
+
+</script>
+
+<svelte:head>
+    <title>Dashboard | Username Title</title>
+</svelte:head>
+<section id="dashboard">
+    <div class="drawer drawer-mobile">
+        <input id="menu-drawer" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content flex flex-col items-center justify-center">
+        <div class="content-container bg-[#fff] w-full h-full">
+          <!-- Page content here -->
+          <div class="navbar bg-[#ffffff] fixed sm:relative z-10 shadow-lg shadow-stone-100 sm:shadow-none justify-between">
+            <div class="nav-content-left hidden sm:flex">
+                Lorem ipsum dolor sit.
+            </div>
+            <div class="flex-none lg:hidden">
+                <label for="menu-drawer" class="btn btn-square btn-ghost">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+                      </svg>
+                </label>
+              </div> 
+            <div class="flex-none">
+              <div class="dropdown dropdown-end">
+                <label tabindex="0" class="btn btn-ghost btn-circle">
+                  <div class="indicator">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                    <span class="badge badge-sm indicator-item">8</span>
+                  </div>
+                </label>
+                <div tabindex="0" class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
+                  <div class="card-body">
+                    <span class="font-bold text-lg">8 Items</span>
+                    <span class="text-info">Subtotal: $999</span>
+                    <div class="card-actions">
+                      <button class="btn btn-primary btn-block">View cart</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="dropdown dropdown-end">
+                <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                  <div class="w-10 rounded-full">
+                    <img src="https://placeimg.com/80/80/people" />
+                  </div>
+                </label>
+                <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                  <li>
+                    <a class="justify-between">
+                      Profile
+                      <span class="badge">New</span>
+                    </a>
+                  </li>
+                  <li><a>Settings</a></li>
+                  <li><a>Logout</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="page-content text-center sm:text-left p-5  h-fit relative top-12 sm:top-0">
+              <slot/>
+          </div>
+        </div>
+        </div> 
+        <div class="drawer-side">
+          <label for="menu-drawer" class="drawer-overlay"></label>
+          <div class="sidebar bg-[#F5F6F7] w-80 text-base-content">
+            <div class="header-container bg-[#2f3640] h-[67px]">
+                <img height="60" width="60" src="/images/dash-logo.png" alt="">
+                <h3 class="text-white text-3xl font-semibold">BeninEats</h3>
+            </div>
+            <ul class="menu p-4">
+                <li class:active={$page.url.pathname == "/dashboard/home"} >
+                    <!-- svelte-ignore a11y-missing-attribute -->
+                    <a href="/dashboard/home">
+                      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+                    <span>Acceuil</span>
+                    </a>
+                </li>
+                <li class:active={$page.url.pathname == "/dashboard/decouverte"}>
+                      <!-- svelte-ignore a11y-missing-attribute -->
+                    <a href="/dashboard/decouverte">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 15.75l-2.489-2.489m0 0a3.375 3.375 0 10-4.773-4.773 3.375 3.375 0 004.774 4.774zM21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                      <span>Découverte</span>
+                    </a>
+                </li>
+                <li class:active={$page.url.pathname == "/dashboard/favoris"}>
+                      <!-- svelte-ignore a11y-missing-attribute -->
+                    <a href="/dashboard/favoris">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                        </svg>
+                      <span>Vos Favoris</span>
+                    </a>
+                </li>
+                <li class:active={$page.url.pathname == "/dashboard/commandes"}>
+                      <!-- svelte-ignore a11y-missing-attribute -->
+                    <a href="/dashboard/commandes">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                        </svg>
+                      <span>Mes Commandes</span>
+                    </a>
+                </li>
+                <li class:active={$page.url.pathname == "/dashboard/messages"}>
+                      <!-- svelte-ignore a11y-missing-attribute -->
+                    <a href="/dashboard/messages">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.068.157 2.148.279 3.238.364.466.037.893.281 1.153.671L12 21l2.652-3.978c.26-.39.687-.634 1.153-.67 1.09-.086 2.17-.208 3.238-.365 1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+                        </svg>
+                      <span>Messages</span>
+                    </a>
+                </li>
+                <li class:active={$page.url.pathname == "/dashboard/parametres"}>
+                      <!-- svelte-ignore a11y-missing-attribute -->
+                    <a href="/dashboard/parametres">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z" />
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                        </svg>
+                      <span>Paramètres</span>
+                    </a>
+                </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+</section>
+<style>
+    .header-container{
+        display: flex;
+        align-items: center;
+        padding: 0px 20px;
+        column-gap: 10px;
+    }
+
+    li{
+        height: 52px;
+        border-radius: 10px;
+    }
+
+    li.active{
+        background-color: #2f3640 ;
+        color: #F7F7F7;
+    }
+    li a{
+        height: 100%;
+    }
+
+    a:focus, a:active{
+        background-color: #1F29371A !important;
+        color: #2f3640;
+    }
+</style>
---
+++ src/routes/dashboard/home/+page.svelte
@@ -0,0 +1,56 @@
+<div class="w-full h-screen flex flex-col gap-y-4">
+    <!-- Liste des partenaires importants -->
+    <div class="w-full h-fit flex flex-col gap-y-4">
+        <div id="resto-header-container" class="h-10 flex justify-between items-center">
+            <h3 id="header-title" class="text-xl text-[#182135] font-semibold">Nos partenaires</h3>
+            <span id="see-more-resto" class="text-sm">Tout voir ></span>
+        </div>
+        <div id="partner-container" class="h-fit w-fit sm:w-full max-w-full grid sm:grid-cols-3 gap-4 overflow-x-auto">
+            <div class="bg-slate-500 h-28 w-72 sm:w-full"></div>
+            <div class="bg-slate-100 h-28 w-72 sm:w-full"></div>
+            <div class="bg-slate-400 h-28 w-72 sm:w-full"></div>
+            <div class="bg-slate-200 h-28 w-72 sm:w-full"></div>
+            <div class="bg-slate-600 h-28 w-72 sm:w-full"></div>
+            <div class="bg-slate-500 h-28 w-72 sm:w-full"></div>
+        </div>
+    </div>
+
+    <!-- Une catégorie -->
+    <div class="w-full h-fit flex flex-col gap-y-4">
+        <div id="resto-header-container" class="h-10 flex justify-between items-center">
+            <h3 id="header-title" class="text-xl text-[#182135] font-semibold">Pizzas</h3>
+            <span id="see-more-resto" class="text-sm">Tout voir ></span>
+        </div>
+        <div id="first-category-container" class="h-fit w-full sm:w-full max-w-full grid sm:grid-cols-3 gap-y-8 sm:gap-4">
+            <div class="bg-slate-500 h-60 sm:h-52 w-full flex "></div>
+            <div class="bg-slate-100 h-60 sm:h-52 w-full"></div>
+            <div class="bg-slate-400 h-60 sm:h-52 w-full"></div>
+            <div class="bg-slate-500 h-60 sm:h-52 w-full"></div>
+        </div>
+    </div>
+    <!-- Deuxie catégorie -->
+    
+    <div class="w-full h-fit flex flex-col gap-y-4">
+        <div id="resto-header-container" class="h-10 flex justify-between items-center">
+            <h3 id="header-title" class="text-xl text-[#182135] font-semibold">Burgers</h3>
+            <span id="see-more-resto" class="text-sm">Tout voir ></span>
+        </div>
+        <div id="first-category-container" class="h-fit w-full sm:w-full max-w-full grid sm:grid-cols-3 gap-y-8 sm:gap-4">
+            <div class="bg-slate-500 h-60 sm:h-52 w-full flex "></div>
+            <div class="bg-slate-100 h-60 sm:h-52 w-full"></div>
+            <div class="bg-slate-400 h-60 sm:h-52 w-full"></div>
+            <div class="bg-slate-500 h-60 sm:h-52 w-full mb-5"></div>
+        </div>
+    </div>
+</div>
+<style>
+        @media screen and (max-width: 960px) {
+            #partner-container{
+                grid-template-columns: repeat(3, 288px);
+            }
+
+            #first-category-container{
+                grid-template-columns: 1fr;
+            }
+        }
+</style>
